75b543266ae5448cf903995bb1e8027ed2a62ec0
Abdur65 <abdurbc@gmail.com>
2024-05-10 17:00:00 +0600

    bug fix

--- MemFIFO.cpp
+++ MemFIFO.cpp
@@ -1,86 +1,108 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> refString{7,0,1,2,0,3,0,4,2,3,0,3,0,3,2,1,2,0,1,7,0,1};
+vector<int> refString{7, 0, 1, 2, 0, 3, 0, 4, 2, 3, 0, 3, 0, 3, 2, 1, 2, 0, 1, 7, 0, 1};
 
 // FIFO algorithm for page demand
 void FIFO()
-{   
+{
     int frameSize = 1;
 
-    while(frameSize != 8)
+    while (frameSize != 8)
     {
         vector<int> frames(frameSize, -1);
         int pageFault = 0;
         int p = 0;
-        for(int i = 0; i < refString.size(); i++)
+        for (int i = 0; i < refString.size(); i++)
         {
-            if(find(frames.begin(), frames.end(), refString[i]) == frames.end())
+            if (find(frames.begin(), frames.end(), refString[i]) == frames.end())
             {
                 pageFault++;
                 frames[p] = refString[i];
                 p++;
-                if(p == frameSize)
+                if (p == frameSize)
                     p = 0;
-                
             }
         }
 
         cout << "For " << frameSize << " frames there are " << pageFault << " page faults." << endl;
 
         frameSize++;
-
     }
-
 }
 
 // Optimal algorithm for page demand
 void Optimal()
 {
-    int frameSize = 1;
+    int frameSize = 3;
 
-    while(frameSize != 8)
+    // while(frameSize != 8)
+    // {
+    vector<int> frames(frameSize, -1);
+    int pageFault = 0;
+    for (int i = 0; i < refString.size(); i++)
     {
-        vector<int> frames(frameSize, -1);
-        int pageFault = 0;
-        int p = 0;
-        for(int i = 0; i < refString.size(); i++)
+        if (find(frames.begin(), frames.end(), refString[i]) == frames.end())
         {
-            vector<bool> visitedPage(100, false);
-            if(find(frames.begin(), frames.end(), refString[i]) == frames.end())
+            pageFault++;
+            if (i < frameSize)
             {
-                pageFault++;
-                if(i < frameSize)
+                frames[i] = refString[i];
+            }
+            else
+            {
+                int lastPage;
+                int pl = 1;
+                vector<bool> visitedPage(10, false);
+                for (int j = i + 1; j < refString.size(); j++)
                 {
-                    frames[i] = refString[i];
-                }
-                else
-                {
-                    int lastPage;
-                    vector<bool> visitedPage(100, false);
-                    for(int j = i; j < refString.size(); j++)
+                    if (find(frames.begin(), frames.end(), refString[j]) != frames.end() and visitedPage[refString[j]] == false)
                     {
-                        if(visitedPage[frames[j]] == false)
+                        lastPage = refString[j];
+                        visitedPage[refString[j]] = true;
+                    }
+                }
+                for (int w = 0; w < frameSize; w++)
+                {
+                    if (visitedPage[frames[w]] == false)
+                    {
+                        //cout << 0 << " ";
+                        frames[w] = refString[i];
+                        pl = 0;
+                        break;
+                    }
+                }
+                if (pl)
+                {
+                    for (int w = 0; w < frameSize; w++)
+                    {
+                        if (frames[w] == lastPage)
                         {
-                            lastPage = j;
-                            visitedPage[frames[j]] = true;
+                            frames[w] = refString[i];
+                            break;
                         }
                     }
-                    frames[lastPage] = refString[i];
                 }
             }
         }
 
-        cout << "For " << frameSize << " frames there are " << pageFault << " page faults." << endl;
+        cout << "PageFault: " << pageFault << " --- ";
+        for (auto u : frames)
+            cout << u << " ";
 
-        frameSize++;
-
+        cout << endl;
     }
+
+    // cout << "For " << frameSize << " frames there are " << pageFault << " page faults." << endl;
+
+    frameSize++;
+
+    // }
 }
 
 int main()
 {
     Optimal();
-    FIFO();
+
     return 0;
 }
